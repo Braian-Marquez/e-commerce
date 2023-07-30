65f03a0de4b3be439b39eed5ef9b6be127cb9590
braian95 <braianmarq95@gmail.com>
2023-07-29 23:01:12 -0300

    primer commit

---
+++ e-commerce/src/test/java/com/logicap/ecommerce/ECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logicap.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
